ab1d604644d29715fb06708a14fff9dc075074f9
mlennard-utn <39415012+mlennard-utn@users.noreply.github.com>
2019-11-04 20:06:25 -0300

    Update tododb-dml.sql

--- scripts/tododb-dml.sql
+++ scripts/tododb-dml.sql
@@ -1,1 +1,2 @@
 CREATE TABLE `tododb`.`tarea` ( `id` INT NOT NULL AUTO_INCREMENT , `descripcion` VARCHAR(200) NOT NULL , `fecha_limite` DATE NOT NULL , `realizada` BOOLEAN NOT NULL , `fecha_realizada` DATE NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB; 
+CREATE TABLE `tododb`.`persona` ( `id` INT NOT NULL AUTO_INCREMENT , `alias` VARCHAR(25) NOT NULL , `nombre` VARCHAR(25) NOT NULL , `apellido` VARCHAR(25) NULL , `email` VARCHAR(50) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB; 
