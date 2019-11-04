50cb6371a76866eeac853883c8bbb95a52030040
mlennard-utn <39415012+mlennard-utn@users.noreply.github.com>
2019-11-04 20:37:09 -0300

    Rename tododb-dml.sql to tododb-ddl.sql

---
+++ scripts/tododb-ddl.sql
@@ -0,0 +1,5 @@
+INSERT INTO `persona` (`id`, `alias`, `nombre`, `apellido`, `email`) VALUES (NULL, 'alumno1', 'alumno', 'uno', 'alumno1@gmail.com'); 
+INSERT INTO `persona` (`id`, `alias`, `nombre`, `apellido`, `email`) VALUES (NULL, 'alumno2', 'alumno', 'dos', 'alumno2@gmail.com'); 
+INSERT INTO `persona` (`id`, `alias`, `nombre`, `apellido`, `email`) VALUES (NULL, 'alumno3', 'alumno', 'tres', 'alumno3@gmail.com'); 
+INSERT INTO `persona` (`id`, `alias`, `nombre`, `apellido`, `email`) VALUES (NULL, 'alumno4', 'alumno', 'cuatro', 'alumno4@gmail.com'); 
+INSERT INTO `persona` (`id`, `alias`, `nombre`, `apellido`, `email`) VALUES (NULL, 'alumno5', 'alumno', 'cinco', 'alumno5@gmail.com'); 
