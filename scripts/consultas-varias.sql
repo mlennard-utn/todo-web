f944e9dfe7665b60cf8de055917b3dca51456e62
mlennard-utn <39415012+mlennard-utn@users.noreply.github.com>
2019-11-04 21:59:11 -0300

    Update consultas-varias.sql

--- scripts/consultas-varias.sql
+++ scripts/consultas-varias.sql
@@ -1,1 +1,7 @@
+--Todas las tareas (descripcion y alias de quien la tiene asignada).
 SELECT descripcion as 'Descripcion de la tarea', alias as 'Persona que la debe realizar' FROM tarea inner join persona on tarea.id_persona = persona.id;
+
+--Todas las tareas que este realizando una persona que tenga la d en su apellido.
+SELECT descripcion as 'Descripcion de la tarea', alias as 'Persona que la debe realizar' FROM tarea inner join persona on tarea.id_persona = persona.id where persona.apellido like '%d%';
+
+
