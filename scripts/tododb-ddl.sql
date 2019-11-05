c351b152ed335f54547c10356545771dbbfae95a
mlennard-utn <39415012+mlennard-utn@users.noreply.github.com>
2019-11-04 21:37:21 -0300

    Update tododb-ddl.sql

--- scripts/tododb-ddl.sql
+++ scripts/tododb-ddl.sql
@@ -3,3 +3,5 @@ INSERT INTO `persona` (`id`, `alias`, `nombre`, `apellido`, `email`) VALUES (NUL
 INSERT INTO `persona` (`id`, `alias`, `nombre`, `apellido`, `email`) VALUES (NULL, 'alumno3', 'alumno', 'tres', 'alumno3@gmail.com'); 
 INSERT INTO `persona` (`id`, `alias`, `nombre`, `apellido`, `email`) VALUES (NULL, 'alumno4', 'alumno', 'cuatro', 'alumno4@gmail.com'); 
 INSERT INTO `persona` (`id`, `alias`, `nombre`, `apellido`, `email`) VALUES (NULL, 'alumno5', 'alumno', 'cinco', 'alumno5@gmail.com'); 
+INSERT INTO `tarea` (`id`, `descripcion`, `fecha_limite`, `realizada`, `fecha_realizada`, `id_persona`) VALUES (NULL, 'Crear repo en github', '2019-11-11', '', '', '1'); 
+
